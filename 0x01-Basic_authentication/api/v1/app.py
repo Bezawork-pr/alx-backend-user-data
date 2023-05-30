ea5fc43ffd85db23c096715eaa70bd379777900c
Bezawork <bezaworkalx@gmail.com>
2023-05-30 09:55:36 +0000

    Create a class BasicAuth that inherits from Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -17,6 +18,8 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 if AUTH_TYPE == "auth":
     auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
