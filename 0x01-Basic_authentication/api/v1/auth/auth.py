ac2567d1e4e23b14d70d2dc0e74b9920cccc293c
Bezawork <bezaworkalx@gmail.com>
2023-05-30 08:13:30 +0000

    Create auth.py to  manage the API authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This file contains class Auth"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class Auth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
+        return None
