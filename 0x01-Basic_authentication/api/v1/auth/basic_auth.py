b2c349790f3bc2823732e6ad55f8b3d83cafdc13
Bezawork <bezaworkalx@gmail.com>
2023-05-30 10:12:49 +0000

    Return Base64 part of the Authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """class BasicAuth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
+        """Return Base 64"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header.startswith("Basic ") is False:
+            return None
+        return authorization_header[6:]
