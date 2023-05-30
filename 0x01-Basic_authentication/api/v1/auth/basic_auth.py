bee7c4e1d1f01624974d760d858aee4104294f8f
Bezawork <bezaworkalx@gmail.com>
2023-05-30 10:54:22 +0000

    Return decoded value of base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This file contains class BasicAuth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
         if authorization_header.startswith("Basic ") is False:
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(
+                                    base64_authorization_header
+                                    ).decode('utf-8')
+        except Exception as NotBase64:
+            return None
