cab28c77edcf67d9dc6ad88525b001ea4e9826d5
Bezawork <bezaworkalx@gmail.com>
2023-05-30 11:34:00 +0000

    Return the User instance based on his email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """This file contains class BasicAuth"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,18 @@ class BasicAuth(Auth):
         index = decoded_base64_authorization_header.find(":")
         decoded = decoded_base64_authorization_header
         return decoded[0: index], decoded[index + 1:]
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password"""
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception as NotFound:
+            return None
+
