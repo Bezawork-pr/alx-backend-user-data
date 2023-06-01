0965a0d43c51ceb0ac820d2ad37bbe0b06a1add8
Bezawork <bezaworkalx@gmail.com>
2023-06-01 12:43:52 +0000

    Retrieve user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,3 +17,12 @@ class SessionAuth(Auth):
         id = str(uuid.uuid4())
         self.user_id_by_session_id[id] = user_id
         return id
+
+    def user_id_for_session_id(self,
+                               session_id: str = None) -> str:
+        """ a User ID based on a Session ID"""
+        if session_id is None:
+            return None
+        if type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
