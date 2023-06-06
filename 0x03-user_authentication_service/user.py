37e8b04f85738083b0e393015601546537a20fd7
Bezawork <bezaworkalx@gmail.com>
2023-06-06 10:00:42 +0000

    Fix pycodestyle errorrs

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
+
 class User(Base):
     """Create class Uses"""
     __tablename__ = "users"
