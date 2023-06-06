31be2a20f539fbefee4f16eb3d381e74d511f134
Bezawork <bezaworkalx@gmail.com>
2023-06-06 12:43:12 +0000

    Define a users function that implements the POST /users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 """Create a Flask app that has a single GET route ("/")"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """Index"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email=email, password=password)
+    except Exception as Error:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
