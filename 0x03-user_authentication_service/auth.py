0d115a927797430fd566a854d36e85cbbbb57b49
Bezawork <bezaworkalx@gmail.com>
2023-06-06 11:28:14 +0000

    Register New User

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """Define a _hash_password method
 that takes in a password string arguments"""
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +13,21 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     return hash
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Instantiate class"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register User"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {email} already exists")
+        except Exception as NewUser:
+            hashed_password = _hash_password(password)
+            self._db.add_user(email=email, hashed_password=hashed_password)
