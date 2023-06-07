1baf1ea1607ce88b7ac96c70420be56b2382351c
Bezawork <bezaworkalx@gmail.com>
2023-06-07 09:01:45 +0000

    Implement a login function to respond to the POST /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Create a Flask app that has a single GET route ("/")"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -28,6 +28,21 @@ def users() -> object:
     except Exception as Error:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> object:
+    """implement a login function to respond
+    to the POST /sessions route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        abort(401)
+    if AUTH.valid_login(email, password) is True:
+        AUTH.create_session(email)
+        return jsonify({"email": email, "message": "logged in"})
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
